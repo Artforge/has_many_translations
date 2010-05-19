b99ddcb2aedd8df4746ba08993f656e82b42519e
matthew jording <mjording@opengotham.com>
2010-05-19 16:19:36 -0400

    fix for attributes on rtranslate

--- lib/has_many_translations/creation.rb
+++ lib/has_many_translations/creation.rb
@@ -152,7 +152,7 @@ module HasManyTranslations
         end
         
         def update_translation!(attrib, loc, origin_locale, options = {})
-           translation_val = @translator.translate(attrib, origin_locale.to_s, loc.to_s).
+           translation_val = @translator.translate(try(attrib), :from => origin_locale.to_s, :to => loc.to_s)
            translations.create(:attribute => attrib, :locale_code => loc.to_s, :value => @translator.translate(translation_val), :locale_name => Google::Language::Languages[loc.to_s], :machine_translation => true, :origin_locale_code => origin_locale ) unless translation_val.nil? || translation_val.match('Error: ')
            
         end
