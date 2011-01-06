e3d5cf7da1e4fb85c146bbd7d8868d2c47982909
matthew jording <mjording@opengotham.com>
2011-01-06 17:50:04 -0500

    8303649, 8303803

--- lib/has_many_translations/hmt_settings.rb
+++ lib/has_many_translations/hmt_settings.rb
@@ -1,10 +1,24 @@
 class HmtSettings
   @@google_api_key = ""
+  @@languages = []
+  @@default_languages = []
+  
   def self.google_api_key=(key)
     @@google_api_key = key
   end
   def self.google_api_key
     @@google_api_key
   end
-  
+  def self.languages=(langs)
+    @@languages = langs
+  end
+  def self.languages
+    @@languages
+  end
+  def self.default_languages=(langs)
+    @@default_languages = langs
+  end
+  def self.default_languages
+    @@default_languages
+  end
 end
